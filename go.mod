a493f78f05f3f388cac2e9677f5a7df2029e12ab
hellgrenj <hellgrenj@gmail.com>
2021-05-01 17:04:41 +0200

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/hellgrenj/monty-hall-simulation
+
+go 1.15
---
+++ main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+
+	fmt.Println("Monty Hall Paradox Simulation")
+	numberOfIterations := 1_000_000
+	var playerChangesDoor bool
+
+	fmt.Printf("\nsimulating the player staying with her original pick (%d iteration)\n", numberOfIterations)
+	playerChangesDoor = false
+	wins := simulate(playerChangesDoor, numberOfIterations)
+	fmt.Printf("player won %d out of %d games when staying with original pick\n", wins, numberOfIterations)
+
+	fmt.Printf("\nsimulating the player changing doors (%d iteration)\n", numberOfIterations)
+	playerChangesDoor = true
+	wins = simulate(playerChangesDoor, numberOfIterations)
+	fmt.Printf("player won %d out of %d games when changing door\n", wins, numberOfIterations)
+}
+
+func simulate(change bool, iterations int) int {
+	wins := 0
+	for i := 0; i < iterations; i++ {
+		doors := []int{1, 2, 3}
+		priceDoor := doors[getRandomDoorIndex(len(doors))]
+		playerSelectedDoor := doors[getRandomDoorIndex(len(doors))]
+		doors = showHostRemovesDoor(doors, priceDoor, playerSelectedDoor)
+		if change {
+			playerSelectedDoor = changeDoor(playerSelectedDoor, doors)
+		}
+		if playerSelectedDoor == priceDoor {
+			wins++
+		}
+
+	}
+	return wins
+}
+func showHostRemovesDoor(doors []int, priceDoor int, playerSelectedDoor int) []int {
+	var rmIndex int
+	for i, v := range doors {
+		if v != priceDoor && v != playerSelectedDoor {
+			rmIndex = i
+			break
+		}
+	}
+	doors = append(doors[:rmIndex], doors[rmIndex+1:]...)
+	return doors
+}
+func changeDoor(currentDoor int, doors []int) int {
+	for _, v := range doors {
+		if currentDoor != v {
+			currentDoor = v
+			break
+		}
+	}
+	return currentDoor
+}
+func getRandomDoorIndex(numberOfDoors int) int {
+	rand.Seed(time.Now().UnixNano())
+	min := 0
+	max := numberOfDoors
+	return rand.Intn(max-min) + min
+}
